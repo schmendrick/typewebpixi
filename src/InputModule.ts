04cbd90727b4cd23cded1a1bbf974ce3b1830c28
felix.schmutz <f.schmutz@lion-entertainment.com>
2017-03-01 09:34:34 +0100

    added keyboard events and made possible to debug via webstorm on watched webpack-dev-server

---
+++ src/InputModule.ts
@@ -0,0 +1,45 @@
+export module InputModule {
+
+    export class Keyboard {
+
+        public keyCode: number;
+        public isDown: boolean;
+        public isUp: boolean;
+
+        public press: () => void;
+        public release: () => void;
+
+        public constructor(keyCode: number) {
+            this.keyCode = keyCode;
+            this.isDown = false;
+            this.isUp = true;
+
+            window.addEventListener(
+                "keydown", this.downHandler.bind(this), false
+            );
+            window.addEventListener(
+                "keyup", this.upHandler.bind(this), false
+            );
+        }
+
+        public downHandler(event: any): void {
+            if (event.keyCode === this.keyCode) {
+                if (this.isUp && this.press)
+                    this.press();
+                this.isDown = true;
+                this.isUp = false;
+            }
+            event.preventDefault();
+        }
+
+        public upHandler(event: any): void {
+            if (event.keyCode === this.keyCode) {
+                if (this.isDown && this.release)
+                    this.release();
+                this.isDown = false;
+                this.isUp = true;
+            }
+            event.preventDefault();
+        };
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {ExampleModule} from "./ExampleClass";
+import {InputModule} from "./InputModule";
 import {} from "pixi.js";
 
 
@@ -20,20 +21,52 @@ function setup() {
 
     stage.addChild(bunny);
 
+    gameLoop();
 }
 
+
+var left: InputModule.Keyboard = new InputModule.Keyboard(37);
+var right: InputModule.Keyboard = new InputModule.Keyboard(39);
+var xOperator: number = 1;
+
+left.press = function () : void {
+  xOperator = -1;
+};
+
+right.press = function () : void {
+    xOperator = 1;
+}
 
 function gameLoop() {
     requestAnimationFrame(gameLoop);
 
-    bunny.x += 1;
-    if (bunny.x > renderer.width)
-        bunny.x = -12;
+    const limit : number = 20;
+
+    if (right.isDown || left.isDown) {
+        if (renderer.backgroundColor == 0x010101)
+            renderer.backgroundColor = 0xAA0000;
+        else {
+            if (right.isDown)
+                renderer.backgroundColor = renderer.backgroundColor+1;
+            else
+                renderer.backgroundColor = renderer.backgroundColor-1;
+        }
+
+    }
+    else
+        renderer.backgroundColor = 0x010101;
+
+    bunny.x += 1 * xOperator;
+    if (bunny.x > renderer.width + limit)
+        bunny.x = -limit;
+
+    if (bunny.x < -limit)
+        bunny.x = renderer.width + limit;
 
     renderer.render(stage);
 }
 
 let example = new ExampleModule.ExampleClass();
-gameLoop();
+
 
 
